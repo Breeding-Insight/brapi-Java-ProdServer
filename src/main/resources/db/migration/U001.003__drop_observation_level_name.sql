0f0dce13a4a1af74a427c49512a9cda77c51b22d
jloux-brapi <jloux.brapi@gmail.com>
2025-05-06 15:18:15 -0400

    Add migration scripts

---
+++ src/main/resources/db/migration/U001.003__drop_observation_level_name.sql
@@ -0,0 +1,16 @@
+ALTER TABLE observation_unit_level
+    DROP COLUMN level_name,
+    ADD COLUMN level_name INTEGER,
+    ADD COLUMN level_order INTEGER;
+
+ALTER TABLE observation_unit_position
+    DROP COLUMN level_name,
+    ADD COLUMN level_name INTEGER,
+    ADD COLUMN level_order INTEGER;
+
+ALTER TABLE study_observation_level
+    DROP COLUMN level_name,
+    ADD COLUMN level_name TEXT,
+    ADD COLUMN level_order INTEGER;
+
+DROP TABLE observation_unit_level_name;;
