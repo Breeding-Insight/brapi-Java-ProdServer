f08118f58df4f2dc3d0fd73c31b30ebc25375b69
Jason Loux <jloux.brapi@gmail.com>
2026-03-30 16:25:51 -0400

    Additional changes to schema

--- src/main/resources/db/migration/U005.004__revert_level_name_schema.sql
+++ src/main/resources/db/migration/U005.004__revert_level_name_schema.sql
@@ -1,3 +1,8 @@
+ALTER TABLE observation_unit_level_name
+ALTER COLUMN level_order DROP NOT NULL;
+
 DROP INDEX lvl_name_program_order_idx;
 
-CREATE UNIQUE INDEX lvl_name_program_id_idx ON observation_unit_level_name (level_name, program_id);
+-- There could be scenarios where the previous unique index is unable to be made.  A non-unique index with the same name
+-- will allow the existing migration to run again in the event of an undo.
+CREATE INDEX lvl_name_program_id_idx ON observation_unit_level_name (level_name, program_id);
