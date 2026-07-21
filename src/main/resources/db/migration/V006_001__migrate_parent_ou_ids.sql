c2afc76cafd3acf1f3cc20514e2883e13c7966d1
Jason Loux <jloux.brapi@gmail.com>
2026-07-21 17:59:40 -0400

    Add some clarifying comments

--- src/main/resources/db/migration/V006_001__migrate_parent_ou_ids.sql
+++ src/main/resources/db/migration/V006_001__migrate_parent_ou_ids.sql
@@ -1,21 +1,23 @@
 WITH
     levels_and_parent_ou_bi_id AS (
 --      Find observation_unit_level codes that contain a key by checking for a space in the level code.
---      Only other level codes that can exist are block/rep
+--      Only other level codes that can exist are block/rep.
+--      Then extract the uuid from the level code, since program key is also contained there.
         SELECT
             id AS level_id,
-            substring(level_code FROM '^([^ ]+)') AS parent_ou_id
+            substring(level_code FROM '^([^ ]+)') AS parent_bi_ou_id
         FROM observation_unit_level
         WHERE level_code LIKE '% %'
     ),
     ou_ids_matched_on_levels AS (
+--      Now match the bi-generated exref ou ids to ex refs ids, and keep observation_unit_level ids for matching in next part
         SELECT
             ou.id AS ou_id,
             levels_and_parent_ou_bi_id.level_id
         FROM observation_unit ou
         JOIN observation_unit_external_references ouex ON ou.id = ouex.observation_unit_entity_id
         JOIN external_reference ex ON ouex.external_references_id = ex.id
-        JOIN levels_and_parent_ou_bi_id ON ex.external_reference_id = levels_and_parent_ou_bi_id.parent_ou_id
+        JOIN levels_and_parent_ou_bi_id ON ex.external_reference_id = levels_and_parent_ou_bi_id.parent_bi_ou_id
     )
 UPDATE observation_unit_level
 SET level_code = regexp_replace(
